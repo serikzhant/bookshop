0fb5b7085a1394e17eaf2b67819c3ed7280440ec
Serikzhan <tlekkaliserik@icloud.com>
2024-07-17 17:38:22 +0600

    Simple OAuth via Github

--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -6,19 +6,21 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='Test Book 1', price='25')
-        book_2 = Book.objects.create(name='Test Book 2', price='55')
+        book_1 = Book.objects.create(name='Test Book 1', price='25', author_name='Author 1')
+        book_2 = Book.objects.create(name='Test Book 2', price='55', author_name='Author 2')
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test Book 1',
-                'price': '25.00'
+                'price': '25.00',
+                'author_name': 'Author 1'
             },
             {
                 'id': book_2.id,
                 'name': 'Test Book 2',
-                'price': "55.00"
+                'price': "55.00",
+                'author_name': 'Author 2'
             },
         ]
         self.assertEqual(data, expected_data)

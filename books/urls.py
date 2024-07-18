e2c6eb8b961c83cda93dc364888684a825a42de5
Serikzhan <tlekkaliserik@icloud.com>
2024-07-18 21:51:18 +0600

    added book and user relations(rate, like, bookmarks) + unittests

--- books/urls.py
+++ books/urls.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.routers import SimpleRouter
 
-from store.views import BookViewSet, auth
+from store.views import BookViewSet, UserBookRelationView, auth
 
 
 router = SimpleRouter()
 
 router.register(r'book', BookViewSet)
+router.register(r'book_relation', UserBookRelationView)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('', include('social_django.urls', namespace='social')),
-    path('auth/', auth)
+    path('auth/', auth),
 ]
 
 urlpatterns += router.urls
--- store/models.py
+++ store/models.py
@@ -6,7 +6,27 @@ class Book(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
 
     def __str__(self):
         return f'{self.id}: {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'OK'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Perfect'),
+        (5, 'Amazing'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.book.name} - rate {self.rate}'
